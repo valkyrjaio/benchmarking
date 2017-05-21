5483eac53528b672bc489f9e7edd4e2f489c7599
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-21 16:01:14 -0700

    Updating output.

--- benchmarking/libs/output.php
+++ benchmarking/libs/output.php
@@ -1,8 +1,11 @@
 <?php
 
-printf(
-    "\n%' 8d:%f:%d",
-    memory_get_peak_usage(),
-    microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
-    count(get_included_files()) - 1
+file_put_contents(
+    '/dev/' . __DIR__,
+    sprintf(
+        "\n%' 8d:%f:%d",
+        memory_get_peak_usage(),
+        microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
+        count(get_included_files()) - 1
+    )
 );
