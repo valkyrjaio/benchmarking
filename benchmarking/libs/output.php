0a01bd07024b837aa1193f717764306de1e96a44
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-21 16:48:32 -0700

    Updating output.

--- benchmarking/libs/output.php
+++ benchmarking/libs/output.php
@@ -5,7 +5,7 @@ if (isset($_GET['benchmark'])) {
     file_put_contents(
         '/var/www/benchmarks/' . explode('/', realpath('./'))[3],
         sprintf(
-            "\n%' 8d:%f:%d\n",
+            "%' 8d:%f:%d",
             memory_get_peak_usage(),
             microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
             // One less than total because this file was included
