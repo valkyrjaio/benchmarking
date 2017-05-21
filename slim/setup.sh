8562f353887f42742d686675b9a7c70520666d92
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-21 15:52:12 -0700

    Fixing setup.

--- slim/setup.sh
+++ slim/setup.sh
@@ -5,5 +5,6 @@ git clone https://github.com/slimphp/Slim-Skeleton.git /var/www/slim/framework
 cd /var/www/slim/framework
 
 composer install --no-dev --optimize-autoloader
+chmod -R 777 logs/
 
 echo "require '/var/www/benchmarking/libs/output.php';" >> /var/www/slim/framework/public/index.php
--- symfony/setup.sh
+++ symfony/setup.sh
@@ -9,7 +9,7 @@ export SYMFONY_ENV=prod
 composer install --no-dev --optimize-autoloader
 
 php bin/console cache:clear --env=prod --no-debug
-chmod o+w -R var/
+chmod -R 777 var/
 chmod o+w var/cache/ var/logs/
 chmod -R o+w var/cache/*
 
