0b33520873912fc23f86a327f16474a42d4b82c8
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-21 15:14:37 -0700

    Fixing silex setup.

--- silex/setup.sh
+++ silex/setup.sh
@@ -6,4 +6,4 @@ cd /var/www/silex/framework
 
 composer install --no-dev --optimize-autoloader
 
-echo "require '/var/www/benchmarking/libs/output.php';" >> /var/www/silex/framework/public/index.php
+echo "require '/var/www/benchmarking/libs/output.php';" >> /var/www/silex/framework/web/index.php
