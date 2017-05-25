25d4660df4cd6c249a5076483357115b0525fa6f
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-24 20:05:30 -0700

    Updating setup.sh for Valkyrja.

--- valkyrja/setup.sh
+++ valkyrja/setup.sh
@@ -4,8 +4,12 @@ git clone https://github.com/valkyrjaio/valkyrja-app.git /var/www/valkyrja/frame
 
 cd /var/www/valkyrja/framework
 
+cp config/EnvExample.php config/Env.php
+
 composer install --no-dev --optimize-autoloader
 
 php valkyrja cache:all
+
+sed -i "17i public const ANNOTATIONS_ENABLED = true;" config/Env.php
 
 echo "require '/var/www/benchmarking/libs/output.php';" >> /var/www/valkyrja/framework/public/index.php
