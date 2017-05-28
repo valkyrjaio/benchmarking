ba9b4368fbdbcda51b4a8c48f1607e6d4351d70d
Melech Mizrachi <melechmizrachi@gmail.com>
2017-05-27 18:37:08 -0700

    Updating output.

--- benchmarking/libs/output.php
+++ benchmarking/libs/output.php
@@ -2,12 +2,15 @@
 
 // Only run if needed
 if (isset($_GET['benchmark'])) {
+    $memory = memory_get_peak_usage();
+    $time = microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'];
+
     file_put_contents(
         '/var/www/benchmarks/' . explode('/', realpath('./'))[3],
         sprintf(
             "%' 8d:%f:%d\n\n%d",
-            memory_get_peak_usage(),
-            microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
+            $memory,
+            $time,
             // One less than total because this file was included
             count(get_included_files()) - 1,
             implode("\n", get_included_files())
