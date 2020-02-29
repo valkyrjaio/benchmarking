7156ada9a4c721637f32634615445a2521cffe25
Melech Mizrachi <melechmizrachi@gmail.com>
2020-02-29 12:13:00 -0700

    Fixing valkyrja install.

--- valkyrja/setup.sh
+++ valkyrja/setup.sh
@@ -4,13 +4,13 @@ git clone https://github.com/valkyrjaio/valkyrja-app.git /var/www/valkyrja/frame
 
 cd /var/www/valkyrja/framework
 
-cp config/EnvExample.php config/Env.php
-rm -f config/EnvExample.php
+cp env/EnvExample.php env/Env.php
+rm -f env/EnvExample.php
 
 composer install --no-dev --optimize-autoloader
 
 php valkyrja optimize
 
-sed -i "17i public const CONFIG_USE_CACHE_FILE = true;" config/Env.php
+sed -i "17i public const CONFIG_USE_CACHE_FILE = true;" env/Env.php
 
 echo "require '/var/www/benchmarking/libs/output.php';" >> /var/www/valkyrja/framework/public/index.php
